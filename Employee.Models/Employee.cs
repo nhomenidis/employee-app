f82dc5812c3dc59efc42ed703abced91091bbe03
Nikos Chomenidis <nhomenidis@me.com>
2018-01-22 23:24:29 +0200

    Create Domain models, Employee and Skill

---
+++ Employee.Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+
+        //navigation property
+        public virtual ICollection<Skill> Skills { get; set; }
+    }
+}
---
+++ Employee.Models/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.Models
+{
+    public class Skill
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; } // for example scientific, social, financial skill.
+
+        public Guid EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
