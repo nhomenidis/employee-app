2e3764489ba43999fd46eebd81dcf2717d831570
Nikos Chomenidis <nhomenidis@me.com>
2018-01-23 00:38:07 +0200

    Create Employee and Skill Repositories

---
+++ Employee.DataContext/Repositories/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.DataContext.Repositories
+{
+    public interface IEmployeeRepository : IRepository<Models.Employee>
+    {
+        
+    }
+    public class EmployeeRepository : BaseRepository<Models.Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(EmployeeDataContext employeeDataContext) : base(employeeDataContext)
+        {
+        }
+    }
+}
---
+++ Employee.DataContext/Repositories/SkillRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Employee.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.DataContext.Repositories
+{
+    public interface ISkillRepository : IRepository<Skill>
+    {
+        
+    }
+    public class SkillRepository : BaseRepository<Skill>, ISkillRepository
+    {
+        public SkillRepository(EmployeeDataContext employeeDataContext) : base(employeeDataContext)
+        {
+        }
+
+        public async Task<IEnumerable<Skill>> GetSkillsByEmployeeId(Guid employeeId)
+        {
+            var skills = await DbSet
+                .Where(s => s.Employee.Id == employeeId)
+                .ToListAsync();
+
+            return skills;
+        }
+    }
+}
