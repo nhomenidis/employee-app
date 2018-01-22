18be510b165ecc265c69c97f8af645c063b847a5
Nikos Chomenidis <nhomenidis@me.com>
2018-01-23 00:39:28 +0200

    Add a DbInitializer for the creation of the Database

---
+++ Employee.DataContext/DbInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.DataContext
+{
+    public class DbInitializer
+    {
+        public static void Initialize(EmployeeDataContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
