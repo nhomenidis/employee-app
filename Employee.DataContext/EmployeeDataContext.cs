3af4746a4699da6edbe2e3f59f9c120b15f881db
Nikos Chomenidis <nhomenidis@me.com>
2018-01-23 00:35:17 +0200

    Create EmployeeDataContext and build fluent API

---
+++ Employee.DataContext/EmployeeDataContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Employee.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.DataContext
+{
+    public class EmployeeDataContext : DbContext
+    {
+        public EmployeeDataContext(DbContextOptions<EmployeeDataContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        // Employee Fluent Api
+            var employee = modelBuilder.Entity<Models.Employee>();
+
+            employee
+                .HasKey(s => s.Id)
+                .HasAnnotation("Unique", true);
+
+            employee
+                .HasMany(c => c.Skills)
+                .WithOne(s => s.Employee)
+                .HasForeignKey(c => c.EmployeeId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            employee
+                .HasIndex(e => new { e.FirstName, e.LastName, e.Email })
+                .IsUnique();
+
+            employee
+                .Property(p => p.FirstName)
+                .HasMaxLength(128)
+                .IsRequired();
+
+            employee
+                .Property(s => s.LastName)
+                .HasMaxLength(128)
+                .IsRequired();
+
+            employee
+                .Property(s => s.Email)
+                .IsRequired();
+            
+         // Skill fluent Api
+
+            var skill = modelBuilder.Entity<Skill>();
+
+            skill
+                .HasKey(s => s.Id)
+                .HasAnnotation("Unique", true);
+
+            skill
+                .HasOne(e => e.Employee)
+                .WithMany(s => s.Skills)
+                .HasForeignKey(s => s.EmployeeId);
+
+            skill
+                .Property(n => n.Name)
+                .HasMaxLength(56)
+                .IsRequired();
+        }
+    }
+}
