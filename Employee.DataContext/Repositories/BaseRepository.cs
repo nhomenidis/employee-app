e4277bf9bffd1ae143ebfdd869f57276928fdbb5
Nikos Chomenidis <nhomenidis@me.com>
2018-01-23 00:37:27 +0200

    Create a BaseRepository and IRepository for common actions to be shared among entities

---
+++ Employee.DataContext/Repositories/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.DataContext.Repositories
+{
+    public abstract class BaseRepository<T> : IRepository<T> where T : class 
+    {
+        protected DbSet<T> DbSet { get; private set; }
+        protected EmployeeDataContext EmployeeDataContext { get; private set; }
+
+        protected BaseRepository(EmployeeDataContext employeeDataContext)
+        {
+            EmployeeDataContext = employeeDataContext;
+            DbSet = employeeDataContext.Set<T>();
+        }
+
+        public virtual Task<T> GetById(Guid id)
+        {
+            return DbSet.FindAsync(id);
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAll()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task<T> Insert(T entity)
+        {
+            var result = DbSet.Add(entity);
+            await EmployeeDataContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public virtual async Task<T> Update(T entity)
+        {
+            var result = DbSet.Update(entity);
+            await EmployeeDataContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public virtual async Task<bool> Delete(Guid id)
+        {
+            var entity = await DbSet.FindAsync(id);
+            DbSet.Remove(entity);
+            await EmployeeDataContext.SaveChangesAsync();
+            return true;
+        }
+
+        public virtual async Task<bool> InsertMany(IEnumerable<T> entities)
+        {
+            DbSet.AddRange(entities);
+            await EmployeeDataContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ Employee.DataContext/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.DataContext.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetById(Guid id);
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Insert(T entity);
+        Task<bool> InsertMany(IEnumerable<T> entities);
+        Task<T> Update(T entity);
+        Task<bool> Delete(Guid id);
+    }
+}
