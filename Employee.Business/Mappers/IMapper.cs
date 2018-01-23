bae169d0c6a14da57028533846a712b9960ade8d
Nikos Chomenidis <nhomenidis@me.com>
2018-01-23 03:07:03 +0200

    Creates IMapper interface with generic Input,Output

---
+++ Employee.Business/Mappers/IMapper.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Employee.Business.Mappers
+{
+    public interface IMapper<in TInput, out TOutput>
+    {
+        TOutput Map(TInput input);
+        IEnumerable<TOutput> Map(IEnumerable<TInput> inputs);
+    }
+}
