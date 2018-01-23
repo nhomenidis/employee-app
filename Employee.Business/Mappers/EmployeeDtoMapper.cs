228cd508ed42596bf68d6e66d827b284c9f22cb2
Nikos Chomenidis <nhomenidis@me.com>
2018-01-23 03:08:07 +0200

    Create EmployeeDtoMapper and SkillDtoMapper

---
+++ Employee.Business/Mappers/EmployeeDtoMapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Employee.Business.DTOs;
+
+namespace Employee.Business.Mappers
+{
+    public class EmployeeDtoMapper : IMapper<Models.Employee,EmployeeDto>
+    {
+        public EmployeeDto Map(Models.Employee employee)
+        {
+            return new EmployeeDto()
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Email = employee.Email,
+                Address = employee.Address,
+                DateOfBirth = employee.DateOfBirth
+            };
+        }
+
+        public IEnumerable<EmployeeDto> Map(IEnumerable<Models.Employee> employees)
+        {
+            return employees.Select(Map);
+        }
+    }
+}
---
+++ Employee.Business/Mappers/SkillDtoMapper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Employee.Business.DTOs;
+using Employee.Models;
+
+namespace Employee.Business.Mappers
+{
+    public class SkillDtoMapper : IMapper<Skill, SkillDto>
+    {
+        public SkillDto Map(Skill skill)
+        {
+            return new SkillDto()
+            {
+                SkillId = skill.Id,
+                EmployeeId = skill.EmployeeId,
+                Name = skill.Name,
+                Category = skill.Category,
+            };
+        }
+
+        public IEnumerable<SkillDto> Map(IEnumerable<Skill> skills)
+        {
+           return skills.Select(Map);
+        }
+    }
+}
