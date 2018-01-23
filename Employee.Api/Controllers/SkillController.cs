aed494b040015762a258a78f1d84c34a870f0b53
Nikos Chomenidis <nhomenidis@me.com>
2018-01-23 12:22:35 +0200

    Add Skill Controller

---
+++ Employee.Api/Controllers/SkillController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Employee.Business.DTOs;
+using Employee.Business.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.Api.Controllers
+{
+    [Route("/skill")]
+    public class SkillController : Controller
+    {
+        private readonly ISkillService _skillService;
+
+        public SkillController(ISkillService skillService)
+        {
+            _skillService = skillService;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateNewSkill([FromBody]CreateSkillDto createSkillDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var newSkillDto = await _skillService.CreateNewSkill(createSkillDto);
+            if (newSkillDto == null)
+            {
+                return NotFound();
+            }
+            return Ok(newSkillDto);
+        }
+
+        [HttpPost("createMany")]
+        public async Task<IActionResult> CreateManyNewSkills([FromBody]IEnumerable<CreateSkillDto> createSkillDtos)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _skillService.CreateManyNewSkills(createSkillDtos);
+
+            if (result == false)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+
+        [HttpDelete("{SkillId}")]
+        public async Task<IActionResult> DeleteSkill(Guid skillId)
+        {
+            var result = await _skillService.DeleteEntity(skillId);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetbyId(Guid id)
+        {
+            var result = await _skillService.GetEntity(id);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _skillService.GetAllEntities();
+            return Ok(result);
+        }
+    }
+}
