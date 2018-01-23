bf3c965409e5ce7e88ef70f677b12fac9560fd91
Nikos Chomenidis <nhomenidis@me.com>
2018-01-23 03:08:52 +0200

    Build CreateEmployeeDtoMapper and CreateSkillDtoMapper

---
+++ Employee.Business/Mappers/CreateEmployeeDtoMapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Employee.Business.DTOs;
+
+namespace Employee.Business.Mappers
+{
+    public class CreateEmployeeDtoMapper : IMapper<CreateEmployeeDto, Models.Employee>
+    {
+        public Models.Employee Map(CreateEmployeeDto employeeDto)
+        {
+            return new Models.Employee()
+            {
+                FirstName = employeeDto.FirstName,
+                LastName = employeeDto.LastName,
+                Email = employeeDto.Email,
+                Address = employeeDto.Address,
+                DateOfBirth = employeeDto.DateOfBirth,
+            };
+        }
+
+        public IEnumerable<Models.Employee> Map(IEnumerable<CreateEmployeeDto> inputs)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Employee.Business/Mappers/CreateSkillDtoMapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Employee.Business.DTOs;
+using Employee.Models;
+
+namespace Employee.Business.Mappers
+{
+    public class CreateSkillDtoMapper : IMapper<CreateSkillDto, Skill>
+    {
+        public Skill Map(CreateSkillDto createSkillDto)
+        {
+            return new Skill()
+            {
+                EmployeeId = createSkillDto.EmployeeId,
+                Name = createSkillDto.Name,
+                Category = createSkillDto.Category,
+            };
+        }
+
+        public IEnumerable<Skill> Map(IEnumerable<CreateSkillDto> createSkillDtos)
+        {
+            return createSkillDtos.Select(Map);
+        }
+    }
+}
