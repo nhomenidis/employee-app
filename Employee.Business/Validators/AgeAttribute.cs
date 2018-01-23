4ff1b9775c3a8b8d5fd711831c37be2f479af4cc
Nikos Chomenidis <nhomenidis@me.com>
2018-01-23 02:03:09 +0200

    Add custom validator for age check

---
+++ Employee.Business/Validators/AgeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Employee.Business.Validators
+{
+    public class AgeAttribute : ValidationAttribute
+    {
+        private int _age;
+
+        public AgeAttribute(int age)
+        {
+            _age = age;
+        }
+        public override bool IsValid(object value)
+        {
+            var dateTime = (DateTime)value;
+            var lowerDateTime = DateTime.Now.AddYears(-_age);
+
+            if (lowerDateTime < dateTime)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
