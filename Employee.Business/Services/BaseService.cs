cd7c0b2d87f2ad3bc3e76620c76e3a35fa17ccd3
Nikos Chomenidis <nhomenidis@me.com>
2018-01-23 12:18:56 +0200

    Create a Base Service and IService

---
+++ Employee.Business/Services/BaseService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Employee.Business.Mappers;
+using Employee.DataContext.Repositories;
+
+namespace Employee.Business.Services
+{
+    public abstract class BaseService<T, TOutput> : IService<T, TOutput> where T : class
+    {
+        private readonly IRepository<T> _repository;
+        private readonly IMapper<T, TOutput> _mapper;
+
+        protected BaseService(IRepository<T> repository, IMapper<T, TOutput> mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<TOutput> GetEntity(Guid id)
+        {
+            var entity = await _repository.GetById(id);
+            var entityDto = _mapper.Map(entity);
+            return entityDto;
+        }
+
+        public async Task<IEnumerable<TOutput>> GetAllEntities()
+        {
+            var entities = await _repository.GetAll();
+            var entitiesDto = _mapper.Map(entities);
+            return entitiesDto;
+        }
+
+        public async Task<TOutput> CreateEntity(T entity)
+        {
+            var createEntity = await _repository.Insert(entity);
+            var createEntityDto = _mapper.Map(createEntity);
+            return createEntityDto;
+        }
+
+        public async Task<bool> DeleteEntity(Guid id)
+        {
+            var deleteEntity = await _repository.Delete(id);
+            return deleteEntity;
+        }
+
+        public async Task<bool> InsertMany(IEnumerable<T> entities)
+        {
+            var result = await _repository.InsertMany(entities);
+            return result;
+        }
+    }
+}
---
+++ Employee.Business/Services/IService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Employee.Business.Services
+{
+    public interface IService<T, TOutput> where T : class
+    {
+        Task<TOutput> GetEntity(Guid id);
+        Task<IEnumerable<TOutput>> GetAllEntities();
+        Task<TOutput> CreateEntity(T entity);
+        Task<bool> DeleteEntity(Guid id);
+        Task<bool> InsertMany(IEnumerable<T> entities);
+    }
+}
